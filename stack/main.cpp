c6c726ec6d6d6ce5e897ba011fbb9fdf04cd04e8
Prakash Rao Vaddina <prakash.vaddina@gmail.com>
2018-12-16 22:52:27 +0100

    Stack implementation with resizing array and linked lists

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,21 +1,210 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <chrono>
+#include <stack>
 #include "stack.h"
+
+namespace T1
+{
+  template <typename T>
+  void POD_Test(const std::string&& dtype)
+  {
+    auto start = std::chrono::steady_clock::now();
+    T st;
+
+    //std::cout << "pushing: ";
+    for (int i = 0; i < 1000000; ++i)
+    {
+      //std::cout << i << " ";
+      st.push(i);
+    }
+    //std::cout << "\n";
+
+    auto sz = st.size();
+    //std::cout << "Popping: ";
+    for (size_t i = 0; i < sz; ++i)
+    {
+      auto& a = st.top();
+      st.pop();
+    }
+    //std::cout << "\n";
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    std::cout << "POD_Test with T=" << dtype << ", took: " << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl;
+  }
+}
+
+namespace T2
+{
+  struct NoCopyData
+  {
+    NoCopyData() : name{}, ids{} {}
+    NoCopyData(std::string&& n, std::vector<int>&& _ids) : name{ std::move(n) }, ids{ std::move(_ids) } {}
+
+    // Copy construction and assignment are deleted
+    NoCopyData(const NoCopyData&) = delete;
+    NoCopyData& operator=(const NoCopyData&) = delete;
+
+    // Move construction and assignment are enabled
+    NoCopyData(NoCopyData&& data)
+    {
+      name = std::move(data.name);
+      ids = std::move(data.ids);
+    }
+    NoCopyData& operator=(NoCopyData&& data)
+    {
+      name = std::move(data.name);
+      ids = std::move(data.ids);
+      return *this;
+    }
+  private:
+    std::string name;
+    std::vector<int> ids;
+  };
+
+  template <typename T>
+  void TestComplexTyp(const std::string&& dtype)
+  {
+    auto start = std::chrono::steady_clock::now();
+    T st;
+    //std::cout << "Pusing objects constructed with rvalue refs, and finally being moved into the stack";
+    for (int i = 0; i < 900000; ++i)
+    {
+      st.push(NoCopyData("name", { i, i + 1, i + 2, i + 3}));
+    }
+    //std::cout << "\n";
+
+    NoCopyData data{ "Same item",{ 437, 438, 439, 440 } };
+    //std::cout << "Pusing one single object using copy assignment into the stack";
+    for (int i = 0; i < 50; ++i)
+    {
+      // Note without the move, the following operation will fail to compile
+      // since the NoCopyData type does not support copying. 
+      // And without the move, push of the stack will try to make a copy of the
+      // passed object
+      // Theoretically at the end of the move operation the data object should
+      // be practically a zombie object with no data inside but for simplicity
+      // it is not implemented here
+      st.push(std::move(data));
+    }
+
+    auto sz = st.size();
+    for (size_t i = 0; i < sz; ++i)
+    {
+      auto& a = st.top();
+      st.pop();
+    }
+
+    //std::cout << "\n";
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    std::cout << "TestComplextyp with T=" << dtype << ", took: " << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl;
+  }
+
+  template <typename T>
+  void ComplexTypeEmplaceTest(const std::string&& dtype)
+  {
+    auto start = std::chrono::steady_clock::now();
+    T st;
+
+    for (int i = 0; i < 500000; ++i)
+    {
+      // Note without the move, the following operation will fail to compile
+      // since the NoCopyData type does not support copying. 
+      // And without the move, push of the stack will try to make a copy of the
+      // passed object
+      // Theoretically at the end of the move operation the data object should
+      // be practically a zombie object with no data inside but for simplicity
+      // it is not implemented here
+      //st.emplace("Same item",{ 437, 438, 439, 440 });
+      st.emplace("Same item",std::vector<int>{ 437, 438, 439, 440 });
+    }
+
+    auto sz = st.size();
+    for (size_t i = 0; i < sz; ++i)
+    {
+      auto& a = st.top();
+      st.pop();
+    }
+
+    //std::cout << "\n";
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    std::cout << "TestComplextyp with T=" << dtype << ", took: " << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl;
+  }
+
+
+}
+
+namespace T3
+{
+  // A simple Data type that implements default copying and moving semantics
+  struct SimpleData
+  {
+    std::string name;
+  };
+
+  template <typename T>
+  void TestSimpleData(const std::string&& dtype)
+  {
+    auto start = std::chrono::steady_clock::now();
+    T st;
+
+    //std::cout << "Pusing objects that are default constructed and finally being moved to the stack";
+    for (int i = 0; i < 1000000; ++i)
+    {
+      // This will invoke the push with move semantics. Since SimpleData implements/supports
+      // default copying and moving this is fine
+      st.push(SimpleData{});
+    }
+    //std::cout << "\n";
+
+    SimpleData data{};
+    //std::cout << "Pusing one single object using copy assignment into the stack";
+    for (int i = 0; i < 1000000; ++i)
+    {
+      st.push(data);
+    }
+
+    auto sz = st.size();
+    for (size_t i = 0; i < sz; ++i)
+    {
+      auto& a = st.top();
+      st.pop();
+    }
+
+    //std::cout << "\n";
+    auto end = std::chrono::steady_clock::now();
+    auto diff = end - start;
+    std::cout << "TestSimpleData with T=" << dtype << ", took: " << std::chrono::duration<double, std::milli>(diff).count() << "ms" << std::endl;
+  }
+}
 
 int main()
 {
-  AG::Stack<int> st;
+  // Plain old data tests with AG::Stack and std::stack
+  T1::POD_Test<AG::Stack<int> >(std::string("AG::Stack<int>"));
+  T1::POD_Test<AG::LLStack<int> >(std::string("AG::LLStack<int>"));
+  T1::POD_Test<std::stack<int> >(std::string("std::stack<int>"));
 
-  for (int i=0; i<100; ++i)
-    st.Push(i);
+  // A complex data type
+  T2::TestComplexTyp<AG::Stack<T2::NoCopyData> >(std::string("AG::Stack<T2::NoCopyData>"));
+  T2::TestComplexTyp<AG::LLStack<T2::NoCopyData> >(std::string("AG::LLStack<T2::NoCopyData>"));
+  T2::TestComplexTyp<std::stack<T2::NoCopyData> >(std::string("std::stack<T2::NoCopyData>"));
 
-  auto sz = st.Size();
-  for (int i = 0; i < sz; ++i)
-  {
-    std::cout << st.Top() << std::endl;
-    st.Pop();
-  }
+  // Use complex data type and push objects via emplace
+  T2::ComplexTypeEmplaceTest<AG::Stack<T2::NoCopyData> >(std::string("AG::Stack<T2::NoCopyData>"));
+  T2::ComplexTypeEmplaceTest<AG::LLStack<T2::NoCopyData> >(std::string("AG::LLStack<T2::NoCopyData>"));
+  T2::ComplexTypeEmplaceTest<std::stack<T2::NoCopyData> >(std::string("std::stack<T2::NoCopyData>"));
+
+  // A simple data type 
+  T3::TestSimpleData<AG::Stack<T3::SimpleData> >(std::string("AG::Stack<T3::SimpleData>"));
+  T3::TestSimpleData<AG::LLStack<T3::SimpleData> >(std::string("AG::LLStack<T3::SimpleData>"));
+  T3::TestSimpleData<std::stack<T3::SimpleData> >(std::string("std::stack<T3::SimpleData>"));
 
   return 0;
 }
+
 
 
--- stack/stack.h
+++ stack/stack.h
@@ -1,78 +1,144 @@
 #pragma once
+#include <memory>
 
 namespace AG
 {
+  ///
+  /// \brief Stack class
+  /// A stack implementation with a resizing array. Meaning the stack is
+  /// initialized with a size of 1. And after every push the size of the array
+  /// is checked and if it is equal to the size of the array, then a new
+  /// array with double the size is created and existing items are copied
+  /// in the new array.
+  /// Similarly when an item is popped, and if the size if 1/4th the size 
+  /// of the array, then a new array with 1/2 the size of the actual array
+  /// is created and used
   template <typename T>
   class Stack
   {
     public:
-      Stack();
-      void Push(T& item);
-      T& Top();
-      void Pop();
-      size_t Size() const;
-#if 0
-      bool IsEmpty() const;
-#endif
-    private:
-      void Resize();
+      Stack() { Resize(1); }
+
+      void push(T& item)
+      {
+        // Copy assignment operation
+        //std::cout << "Copy pushing ....\n";
+        mArray[mLen++] = item;
+        if (mLen == mMaxSize)
+        {
+          Resize(mMaxSize * 2);
+        }
+      }
+      void push(T&& item)
+      {
+        // Move assignment operation
+        //std::cout << "Move pushing ....\n";
+        mArray[mLen++] = std::move(item);
+        if (mLen == mMaxSize)
+        {
+          Resize(mMaxSize * 2);
+        }
+      }
+      template <typename... Ts>
+      void emplace(Ts&&... args)
+      {
+        mArray[mLen++] = std::move(T(std::forward<Ts>(args)...));
+        if (mLen == mMaxSize)
+        {
+          Resize(mMaxSize * 2);
+        }
+      }
+      T& top()
+      {
+        return mArray[mLen - 1];
+      }
+      void pop()
+      {
+        if (mLen > 0)
+        {
+          --mLen;
+          if (mLen > 0 && mLen == mMaxSize / 4)
+          {
+            Resize(mMaxSize / 2);
+          }
+        }
+      }
+      size_t size() const { return mLen; }
+      bool empty() const { return mLen == 0; }
+  private:
+      void Resize(const size_t newSz)
+      {
+        auto oldArray = mArray;
+        mMaxSize = newSz;
+        // Call to default constructor
+        mArray = new T[mMaxSize];
+        for (size_t i = 0; i < mLen; ++i)
+        {
+          // Move assignment operation
+          mArray[i] = std::move(oldArray[i]);
+        }
+      }
+
     private:
       T * mArray = nullptr;
-      size_t mN = 0;
+      size_t mLen = 0;
       size_t mMaxSize = 0;
   };
 
+  ///
+  /// \brief LLStack class
+  /// A stack implementation with a linked list. Here a simple node structure
+  /// represents each item that is pushed on the stack.
+  /// The node holds a link to the last item in the list. The first item
+  /// of the stack does not point to anything else.
   template <typename T>
-  void Stack<T>::Resize()
+  class LLStack
   {
-    if (mN == mMaxSize)
+  public:
+    LLStack() {}
+    void push(T& item)
     {
-      auto oldArray = mArray;
-      auto oldSz = mMaxSize;
-      mMaxSize = oldSz * 2;
-      mArray = new T[mMaxSize];
-
-      for (size_t i = 0; i < oldSz; ++i)
-        mArray[i] = oldArray[i];
+      auto lastNode = mLast;
+      mLast = new Node<T>{};
+      mLast->item = item;
+      mLast->pPrevItem = lastNode;
+      ++mLen;
     }
-   }
-
-  template <typename T>
-  Stack<T>::Stack()
-  {
-    auto sz = 1;
-    mArray = new T[sz];
-    mMaxSize = sz;
-  }
-
-  template <typename T>
-  void Stack<T>::Push(T& item)
-  {
-    mArray[mN++] = item;
-    Resize();
-  }
-
-  template<typename T>
-  T& Stack<T>::Top()
-  {
-    return mArray[mN-1];
-  }
-
-  template<typename T>
-  void Stack<T>::Pop()
-  {
-    if (mN > 0)
+    void push(T&& item)
     {
-      --mN;
+      auto lastNode = mLast;
+      mLast = new Node<T>{};
+      mLast->item = std::move(item);
+      mLast->pPrevItem = lastNode;
+      ++mLen;
     }
-  }
-
-  template<typename T>
-  size_t Stack<T>::Size() const
-  {
-    return mN;
-  }
-
+    template <typename... Ts>
+    void emplace(Ts&&... args)
+    {
+      push(T{ std::forward<Ts>(args)... });
+    }
+    T& top() { return mLast->item; }
+    void pop()
+    {
+      if (mLen > 0)
+      {
+        auto node2Del = mLast;
+        mLast = mLast->pPrevItem;
+        delete node2Del;
+        --mLen;
+      }
+    }
+    size_t size() const { return mLen; }
+    bool empty() const { return mLen == 0; }
+  private:
+    template <typename U> struct Node
+    {
+      U item;
+      Node<U>* pPrevItem;
+    };
+    Node<T>* mLast = nullptr;
+    size_t mLen = 0;
+  };
 }
 
 
